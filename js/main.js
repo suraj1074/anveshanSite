e652a0e4c3ae793ad8f0751518af3f5bb96088dd
Suraj Jha <jhsuraj@amazon.com>
2019-06-08 21:26:28 +0530

    newsletter basics

--- js/main.js
+++ js/main.js
@@ -288,8 +288,27 @@
 	});
 	$('#book_time').timepicker();
 
+  $( ".subscribe-form" ).submit(function( event ) {
+  		event.preventDefault();
+  		var user_email = $("input[name='subscription-email'").val();
+  		if(!isValidEmailAddress(user_email)){
+  			alert('not a valid e-mail address');
+  		}
+  		else{
+  			$.post( "newsletter.php", { subscription-email : user_email })
+			  .done(function( data ) {
+			    alert( "You subscribed." );
+			  })
+			  .fail(function(msg) {
+    			alert( "Failed to subscribe." );
+  			});
+  		}
+  });
 
-
+  function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+  }
 
 })(jQuery);
 
---
+++ newsletter.php
@@ -0,0 +1,50 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+   exit;
+}
+
+$user_email = $_POST["subscription-email"];
+if(IsNullOrEmptyString($user_email)){
+	die('email is empty(var)');
+}
+if(!IsValidEmail($user_email)){
+	die('email not valid');
+}
+
+$count = 0;
+$timeout_secs = 10; //number of seconds of timeout
+$got_lock = true;
+
+$my_file = 'newsletter-subscription.txt';
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file); //implicitly creates file
+
+while (!flock($handle, LOCK_EX | LOCK_NB, $wouldblock)) {
+    if ($wouldblock && $count++ < $timeout_secs) {
+        sleep(1);
+    } else {
+        $got_lock = false;
+        break;
+    }
+}
+if ($got_lock) {
+	$to_write = date("M,d,Y h:i:s A"). "\t" . $user_email;
+	fwrite($handle, $to_write);
+	fclose($handle);
+}
+
+function IsNullOrEmptyString($str){
+    return (!isset($str) || trim($str) === '');
+}
+function IsValidEmail($email){
+	$regex = /^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix;
+	if (!preg_match($regex, $username))
+	{
+ 		return FALSE;
+	}
+	else
+	{
+ 		return TRUE;
+	}
+}
+?>
